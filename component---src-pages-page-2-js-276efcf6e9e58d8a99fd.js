5e669cff1f7bd32b714a71075c6ac66c651b4042
Mathieu Breton <mathieubre@gmail.com>
2018-11-20 00:42:37 +0100

    Updates

---
+++ component---src-pages-page-2-js-276efcf6e9e58d8a99fd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{137:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(149),l=t(147);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-276efcf6e9e58d8a99fd.js.map

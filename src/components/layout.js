955ae39dcfa3c43daf78879fdd7fe9178f3a01f4
Mathieu Breton <mathieubre@gmail.com>
2018-12-03 16:07:59 +0100

    add disabling google analytic and hotjar in dev mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,6 @@ import Footer from './footer'
 import './commons/styles/knacss.css'
 import './commons/styles/layout.css'
 import './commons/styles/octave-font.css'
-
 const Layout = ({ children, hideCallToAction }) => (
   <StaticQuery
     query={graphql`
@@ -35,30 +34,8 @@ const Layout = ({ children, hideCallToAction }) => (
           >
             <base href={process.env.BASE_URL} />
             <html lang="fr" />
-            <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=UA-126067793-2"
-            />
-            <script>
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'UA-126067793-2');`}
-            </script>
-            <script>
-              {`
-                (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:1096949,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              `}
-            </script>
+            {process.env.NODE_ENV === 'production' &&
+              hotJarAndGoogleAnalyticsTags()}
           </Helmet>
           <Header siteTitle={title} hideCallToAction={hideCallToAction} />
           {children}
@@ -68,6 +45,40 @@ const Layout = ({ children, hideCallToAction }) => (
     }}
   />
 )
+
+function hotJarAndGoogleAnalyticsTags() {
+  let i = 0
+  console.log(
+    `\n===============> Adding Google Analytics & HotJar tags ! <===============`
+  )
+  return [
+    <script
+      key={i++}
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-126067793-2"
+    />,
+    <script key={i++}>
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-126067793-2');`}
+    </script>,
+    <script key={i++}>
+      {`
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:1096949,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `}
+    </script>,
+  ]
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
